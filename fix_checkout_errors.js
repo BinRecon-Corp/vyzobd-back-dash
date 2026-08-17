7ece54a7765104addd2f3fbf066227e715f5cd34
shuvo-halder <samshuvo00@gmail.com>
2026-08-18 01:33:27 +0600

    feat(clean): clean projects

--- fix_checkout_errors.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'src/backend/services/storefront/checkout.service.ts';
-let code = fs.readFileSync(file, 'utf8');
-
-code = code.replace(/throw new AppError\((.+?), 400, "PRODUCT_UNAVAILABLE"\);/g, 'throw new AppError($1, 404, "PRODUCT_UNAVAILABLE");');
-code = code.replace(/throw new AppError\((.+?), 400, "VARIANT_UNAVAILABLE"\);/g, 'throw new AppError($1, 404, "VARIANT_UNAVAILABLE");');
-code = code.replace(/throw new AppError\((.+?), 400, "INSUFFICIENT_STOCK"\);/g, 'throw new AppError($1, 409, "INSUFFICIENT_STOCK");');
-
-fs.writeFileSync(file, code);
--- patch_checkout_controller_obj.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'src/backend/controllers/storefront/checkout.controller.ts';
-let code = fs.readFileSync(file, 'utf8');
-
-code = code.replace(/const \{ paymentMethod, clientId, sessionId \} = req\.body;/g, 'const { paymentMethod, clientId, sessionId, shippingAddress, billingAddress } = req.body;');
-code = code.replace(/StorefrontCheckoutService\.completeCheckout\(identifier, paymentMethod, clientId, sessionId\)/g, 'StorefrontCheckoutService.completeCheckout(identifier, paymentMethod, clientId, sessionId, shippingAddress, billingAddress)');
-
-fs.writeFileSync(file, code);
